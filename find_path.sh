0a44213488f9a8e7383dcfb928a06481a36b1a65
Rohith Kapse C <34828331+rohith9499@users.noreply.github.com>
2020-08-31 11:57:08 +0530

    Update find_path.sh

--- find_path.sh
+++ find_path.sh
@@ -5,6 +5,12 @@ then
 	echo "Usage: Pass 2 arguments"
 	exit 0
 fi
-	find $1 -type f -exec file {} + | grep ELF;
+	find $1 -type f -exec file {} + | grep elf| cut -d" " -f1 > file.txt;
+	cat file.txt | cut -d":" -f1 >file2.txt
+for i in $(cat file2.txt)
+do
+	NUM=$i
+	size $NUM
+done
 
 
